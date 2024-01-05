64acefa3ebfd7db76a76fd10a245d5c76ed307c7
Eusuf Uddin <eusuf15-8926@diu.edu.bd>
2024-01-05 21:03:10 +0600

    partially done

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -11,11 +11,7 @@ class _HomePageState extends State<HomePage> {
   final Color bgColor = Color(0xff0E0E0E);
   final Color cardColor = Color(0xff28293D);
   bool isClicked = false;
-
-
-
-
-
+  int _value = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -29,70 +25,112 @@ class _HomePageState extends State<HomePage> {
         color: bgColor,
         child: Column(
           children: [
-            SizedBox(height: 10,),
+            SizedBox(
+              height: 10,
+            ),
             Expanded(
                 flex: 3,
                 child: Row(
                   children: [
                     Expanded(
                         child: InkWell(
-                          onTap: (){
-                            setState(() {
-                              isClicked = true;
-                            });
-                          },
-                          child: Card(
-                            color: isClicked != true ? Colors.cyan: cardColor,
-                            child: Column(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                               children: [
-                                 Icon(Icons.male, size: 40,color: isClicked==false ? Colors.white: Colors.white),
-                                 Text("Male",style: TextStyle(color: Colors.white),)
-                               ],
-                                                ),
-                                              ),
-                        )),
+                      onTap: () {
+                        setState(() {
+                          isClicked = true;
+                        });
+                      },
+                      child: Card(
+                        color: isClicked != true ? Colors.cyan : cardColor,
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Icon(Icons.male,
+                                size: 40,
+                                color: isClicked == false
+                                    ? Colors.white
+                                    : Colors.white),
+                            Text(
+                              "Male",
+                              style: TextStyle(color: Colors.white),
+                            ),
+                          ],
+                        ),
+                      ),
+                    )),
                     Expanded(
                         child: InkWell(
-                          onTap: (){
-                            setState(() {
-                              isClicked = false;
-                            });
-                          },
-                          child: Card(
-                            color: isClicked != false ? Colors.cyan: cardColor,
-                            child: Column(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: [
-                                Icon(Icons.female, size: 40,color: isClicked==false ? Colors.white: Colors.white),
-                                Text("Female",style: TextStyle(color: Colors.white),)
-                              ],
-                            ),
-                          ),
-                        ))
+                      onTap: () {
+                        setState(() {
+                          isClicked = false;
+                        });
+                      },
+                      child: Card(
+                        color: isClicked != false ? Colors.cyan : cardColor,
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Icon(Icons.female,
+                                size: 40,
+                                color: isClicked == false
+                                    ? Colors.white
+                                    : Colors.white),
+                            Text(
+                              "Female",
+                              style: TextStyle(color: Colors.white),
+                            )
+                          ],
+                        ),
+                      ),
+                    ))
                   ],
                 )),
             Expanded(
                 flex: 3,
-                child: Container(
-                  height: MediaQuery.of(context).size.height,
-                  width: MediaQuery.of(context).size.width,
-                  margin: EdgeInsets.fromLTRB(6, 10, 6, 10),
-                  decoration: BoxDecoration(color: cardColor, borderRadius: BorderRadius.circular(16)),
+                child: Card(
+                  color: cardColor,
                   child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
                     children: [
-                      Text("Height", style: TextStyle(color: Colors.white, fontSize: 24)),
-                       Row(
-                         textBaseline: TextBaseline.alphabetic,
-                         children: [
-                           Text("120", style: TextStyle(color: Colors.white, fontSize: 26, fontWeight: FontWeight.bold),),
-                           Text("cm", style: TextStyle(color: Colors.white, fontSize: 14, fontWeight: FontWeight.normal),),
-                         ],
-                       )
+                      Text("Height",
+                          style: TextStyle(color: Colors.white, fontSize: 24)),
+                      Row(
+                        crossAxisAlignment: CrossAxisAlignment.baseline,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        textBaseline: TextBaseline.alphabetic,
+                        children: [
+                          Text(
+                            "${_value}",
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 26,
+                                fontWeight: FontWeight.bold),
+                          ),
+                          SizedBox(
+                            width: 2,
+                          ),
+                          Text(
+                            "cm",
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 14,
+                                fontWeight: FontWeight.normal),
+                          ),
+                        ],
+                      ),
+                      Slider(
+                          min: 0,
+                          max: 200,
+                          thumbColor: Colors.cyan,
+                          activeColor: Colors.pink,
+                          inactiveColor: Colors.blueGrey,
+                          value: _value.toDouble(),
+                          onChanged: (double newValue) {
+                            setState(() {
+                              _value = newValue.toInt();
+                            });
+                          })
                     ],
                   ),
-
-
                 )),
             Expanded(
                 flex: 3,
